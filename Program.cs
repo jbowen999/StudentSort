6a01e84f69a92b89d17322a1608b365302fb1e6b
jbowen999 <julian.e.bowen@outlook.com>
2024-07-19 10:31:13 -0700

    Still need to add merge sort.

--- Program.cs
+++ Program.cs
@@ -26,10 +26,10 @@
                     BubbleSort(students);
                     break;
                 case "2":
-                    // Selection Sort
+                    SelectionSort(students);
                     break;
                 case "3":
-                    // Insertion Sort
+                    InsertionSort(students);
                     break;
                 default:
                     // none of these cases match
@@ -44,7 +44,7 @@
         {
             foreach (var item in arr)
             {
-                Console.Write($"{item.name}: {item.gpa} | ");
+                Console.WriteLine($"{item.name}: {item.gpa} ");
             }
             Console.WriteLine();
         }
@@ -65,6 +65,60 @@
                     }
                 }
             }
+            Console.WriteLine("Students sorted by GPA (Bubble Sort)");
+
+        }
+
+        public static void SelectionSort(Student[] arrToSort)
+        {
+            // minIndex keeps track of the smallest index in each iteration  
+            // temp is used as temporary storage  
+            int minIndex; 
+            Student temp;
+
+            // O(n) how many times we need to go though the unsorted array  
+            for (int i = 0; i < arrToSort.Length; i++)
+            {
+                minIndex = i; // set the minIdex equal to current smallest index  
+                for (int j = i; j < arrToSort.Length; j++) // loop through each element starting at i  
+                {
+                    // if the element is smaller than the current minIndex  
+                    if (arrToSort[j].gpa > arrToSort[minIndex].gpa)
+                    {
+                        // swap  
+                        minIndex = j;
+                    }
+                }
+
+                // swap elements  
+                // swap current i (which is smallest position with the smallest/min element)  
+                temp = arrToSort[i];
+                arrToSort[i] = arrToSort[minIndex];
+                arrToSort[minIndex] = temp;
+            }
+            Console.WriteLine("Students sorted by GPA (Selection Sort)");
+
+        }
+
+        public static void InsertionSort(Student[] arr)
+        {
+            // Overall worst case: O(n^2) == array is reverse sorted
+            // Best case: O(n) == while loop never runs
+            for (int i = 1; i < arr.Length; i++) // O (n)
+            {
+                Student temp = arr[i];// store the current element as it might be overwritten
+                int priorIndex = i - 1; // start comparing with element before the current element
+
+                while (priorIndex >= 0 && arr[priorIndex].gpa < temp.gpa) // O (n)
+                {
+                    arr[priorIndex + 1] = arr[priorIndex];
+                    priorIndex--;
+                }
+
+                // need an assignment
+                arr[priorIndex + 1] = temp;
+            }
+            Console.WriteLine("Students sorted by GPA (Insertion Sort)");
         }
     }
 }
